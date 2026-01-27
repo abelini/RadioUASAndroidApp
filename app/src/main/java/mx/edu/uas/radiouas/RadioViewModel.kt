376a4a91402907c9aa9d688ff196e3fdf6f09410
Ing. Abel Bottello <abel@galaxyweb.dev>
2026-01-27 13:20:23 -0700

    test

--- app/src/main/java/mx/edu/uas/radiouas/RadioViewModel.kt
+++ app/src/main/java/mx/edu/uas/radiouas/RadioViewModel.kt
@@ -7,9 +7,17 @@ import androidx.media3.exoplayer.ExoPlayer
 
 class RadioViewModel(application: Application) : AndroidViewModel(application) {
 
+
     // Inicializamos ExoPlayer de forma única
     val player: ExoPlayer = ExoPlayer.Builder(application).build()
 
+    init {
+        player.addListener(object : androidx.media3.common.Player.Listener {
+            override fun onPlayerError(error: androidx.media3.common.PlaybackException) {
+                println("EXOPLAYER_ERROR: ${error.message}")
+            }
+        })
+    }
     fun reproducirAudio(url: String) {
         val mediaItem = MediaItem.fromUri(url)
         player.setMediaItem(mediaItem)
